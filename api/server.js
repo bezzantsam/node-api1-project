f7db6edb212c7d8b63ed1644543c871a085170e1
bezzantsam <83626945+bezzantsam@users.noreply.github.com>
2022-03-07 17:17:24 -0800

    put

--- api/server.js
+++ api/server.js
@@ -7,17 +7,55 @@ const server = express()
 server.use(express.json())
 
 
-server.delete('/api/users/:id', async (req, res) => {
-  const possibleUser = await User.findById(req.params.id)
-  if (!possibleUser) {
-      res.status(404).json({
-          message: "The user with the specified ID does not exist"
-      })
-  }else {
-      const deletedUser = await User.remove(req.params.id)
-      res.status(200).json(deletedUser)
-  }
+server.put('/api/users/:id', async (req, res) => {
+    try{
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+            })
+        } else { 
+            if (!req.body.name || !req.body.bio){
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user',
+                })
+            }else {
+              const updatedUser =  await  User.update(req.params.id, req.params.body)
+              res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "error updating user",
+            err: err.message,
+            stack: err.stack
+      
+    })  
+    }
 })
+
+
+server.delete('/api/users/:id', async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist"
+        })
+    }else {
+        const deletedUser = await User.remove(req.params.id)
+        res.status(200).json(deletedUser)
+    }
+    } catch (err) {
+        res.status(500).json({
+            message: "There was an error while saving the user to the database",
+            err: err.message,
+            stack: err.stack
+      
+    })  
+    }
+  }
+)
 
 
 server.post('/api/users', (req, res) => {
