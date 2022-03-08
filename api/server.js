29ae9feabeccfc7b1033fb4e3e351635411d9119
bezzantsam <83626945+bezzantsam@users.noreply.github.com>
2022-03-07 17:42:31 -0800

    16/16 mvp

--- api/server.js
+++ api/server.js
@@ -15,12 +15,12 @@ server.put('/api/users/:id', async (req, res) => {
                 message: "The user with the specified ID does not exist",
             })
         } else { 
-            if (!req.body.name || !req.body.bio){
+            if (!req.body.name || !req.body.bio) {
                 res.status(400).json({
                     message: 'Please provide name and bio for the user',
                 })
             }else {
-              const updatedUser =  await  User.update(req.params.id, req.params.body)
+              const updatedUser =  await  User.update(req.params.id, req.body,)
               res.status(200).json(updatedUser)
             }
         }
