fa1067253e2351929b9c926586c1b3f5762f5e12
bezzantsam <83626945+bezzantsam@users.noreply.github.com>
2022-03-07 16:17:49 -0800

    .post working

--- api/server.js
+++ api/server.js
@@ -4,6 +4,30 @@ const express = require('express')
 const User = require('./users/model')
 
 const server = express()
+server.use(express.json())
+
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name ||  !user.bio){
+        res.status(422).json({
+            message: 'name and bio required'
+        })
+    } else {
+    User.insert(user)
+      .then(newlyCreatedUser => {
+       res.status(201).json(newlyCreatedUser)
+    })
+        .catch(err => {
+            res.status(500).json({
+                message: "error creating users",
+                err: err.message,
+                stack: err.stack
+          
+        })  
+      })
+    }
+})
 
 server.get('/api/users', (req, res) => {
     User.find()
